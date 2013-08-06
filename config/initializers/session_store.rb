ee2892e85581275bee89c9c903b3a7060dd721b4
Polar Humenn <polar@adiron.com>
2013-08-06 17:37:58 -0400

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Glpf::Application.config.session_store :cookie_store, key: '_glpf_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Glpf::Application.config.session_store :active_record_store
