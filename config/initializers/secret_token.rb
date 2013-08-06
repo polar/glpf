afa410a0a913439bd57185047ef2415fa448282a
Polar Humenn <polar@adiron.com>
2013-08-06 17:37:58 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glpf::Application.config.secret_token = 'a6154f2e25c52a3ae731d6e3cee723d1be1086df83a09aabee5276dc36c4deac5d7df70cb322a18460d419498541cc28ce101caf3a9d6dc8abfbfabb11db1688'
