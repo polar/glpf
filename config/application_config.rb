6cc97839b31a7b3817b5c40f33c1741fd9912cc3
Polar Humenn <polar@adiron.com>
2013-08-08 15:40:34 -0400

    Merge branch 'master' of github.com:polar/glpf
    Conflicts:
    	Gemfile
    	config/application.rb
    	config/environments/production.rb
    	config/locales/en.yml

--- config/application_config.rb
+++ config/application_config.rb
@@ -1,3 +1,7 @@
 
 configatron.community_name = "AgModels Great Lakes Protection Fund Portal"
 configatron.community_tagline = "Protecting Our Water."
+configatron.support_email = "polar@adiron.com"
+configatron.app_host = "agmodels-glpf.herokuapp.com"
+configatron.meta_description = "AgModels works with the Great Lakes Protection Func"
+configatron.meta_tags = "Water Dairy Feed Cows Bovine Heifer Milk Farm Agriculture"
