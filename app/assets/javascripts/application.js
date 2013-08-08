39859d21ba7940af2864758dd5f1c9015c202d74
Polar Humenn <polar@adiron.com>
2013-08-08 14:53:23 -0400

    Merge branch 'master' of github.com:polar/glpf
    Conflicts:
    	Gemfile
    	config/application.rb
    	config/environments/production.rb
    	config/locales/en.yml

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require community_engine
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Glpf::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, glpf.css.erb, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js community_engine.css community_engine.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
